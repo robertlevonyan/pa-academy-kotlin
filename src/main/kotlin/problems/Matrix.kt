01b8a6bde4d04362c2915e0591fd829a557aa9f7
Robert <bigbob1991@gmail.com>
2023-06-06 20:51:16 +0400

    f501

--- src/main/kotlin/problems/Matrix.kt
+++ src/main/kotlin/problems/Matrix.kt
@@ -7,10 +7,15 @@ fun createMatrix(size: Int): Array<Array<Int>> {
 //        }
 //    }
 
+//    val array = arrayOf(
+//        arrayOf(1, 5, 8, 3),
+//        arrayOf(2, 0, 7, 8),
+//        arrayOf(3, 4, 9, 5),
+//    )
     val array = arrayOf(
-        arrayOf(1, 5, 8, 3),
-        arrayOf(2, 0, 7, 8),
-        arrayOf(3, 4, 9, 5),
+        arrayOf(1, 8, 5),
+        arrayOf(0, 2, 7),
+        arrayOf(3, 4, 9),
     )
 
     return array
@@ -24,6 +29,22 @@ fun f491(matrix: Array<Array<Int>>): Array<Array<Int>> {
 
     return matrix.swapColumns(0, maxColumnIndex)
         .swapRows(0, maxRowIndex)
+}
+
+fun f501(matrix: Array<Array<Int>>): Array<Int> {
+    return matrix.mapIndexed { indexRow, ints ->
+        var sum = 1
+        var indexSum = 0
+        ints.forEachIndexed { indexColumn, i ->
+            if (i % 2 == 0) {
+                if (indexColumn == indexRow && i <= ints[indexRow]) {
+                    indexSum += indexColumn
+                    sum *= i
+                }
+            }
+        }
+        if (indexSum % 2 != 0) sum else 1
+    }.toTypedArray()
 }
 
 fun Array<Array<Int>>.swapRows(first: Int, second: Int): Array<Array<Int>> {
@@ -43,11 +64,12 @@ fun Array<Array<Int>>.swapColumns(first: Int, second: Int): Array<Array<Int>> {
 }
 
 fun main() {
-    val matrix = createMatrix(4)
+    val matrix = createMatrix(10)
     println(matrix.joinToString("\n") { it.contentToString() })
     println("---")
 //    println(f452(matrix))
-    println(f491(matrix).joinToString("\n") { it.contentToString() })
+//    println(f491(matrix).joinToString("\n") { it.contentToString() })
+    println(f501(matrix).joinToString(", "))
 }
 
 fun f452(matrix: Array<Array<Int>>): List<Int> {
